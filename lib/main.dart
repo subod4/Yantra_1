50cea2fc0a5095924dd9a861bf556e441d345fff
Mohit Adhikari <adhikari.mhtp@gmail.com>
2024-06-14 22:02:39 +0545

    sucessfully navigated to customer home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:yantra/pages/customer_home.dart';
 import 'package:yantra/pages/home.dart';
 import 'package:yantra/pages/profile.dart';
 import 'package:yantra/pages/qrGenerator.dart';
@@ -21,32 +22,41 @@ class MyApp extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: 50,
-              width: 370,
-              color: Colors.black, // Add a background color to make it visible
-              child: Center(
-                child: Text(
-                  "Sign in as?",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+
+               Container(
+                height: 50,
+                width: 370,
+
+                color: Colors.black, // Add a background color to make it visible
+                child: Center(
+                  child: Text(
+                    "Sign in as?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
+
             SizedBox(height: 20), // Add some spacing between the containers
-            Container(
-              height: 200,
-              width: 300,
-              color: Colors.blue,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Icon(
-                  Icons.car_rental,
-                  color: Colors.white,
-                  size: 100,
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => CustomerHome()));
+              },
+              child: Container(
+                height: 200,
+                width: 300,
+                color: Colors.blue,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Icon(
+                    Icons.car_rental,
+                    color: Colors.white,
+                    size: 100,
+                  ),
                 ),
               ),
             ),
